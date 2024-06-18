27481361d08619f567a4c81af0dd496b71c5d16d
Grigoriy-F <grinfed11@gmail.com>
2024-06-18 20:34:27 +0300

    Из говна и палок

---
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -0,0 +1,20 @@
+package ru.kata.spring.boot_security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@SpringBootApplication
+public class SpringBootSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDao.java
@@ -0,0 +1,12 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import ru.kata.spring.boot_security.demo.model.Role;
+
+import java.util.List;
+
+public interface RoleDao {
+    Role findByRole (String role);
+    void save (Role role);
+    boolean exist(String role);
+
+}
